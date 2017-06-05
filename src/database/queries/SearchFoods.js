da2588b11afc5a22d76b3c1c7bb37ece798bc7a7
Chris <christophersstjohn@gmail.com>
2017-06-05 11:42:41 +0800

    New Mongoose Queries

---
+++ src/database/queries/SearchFoods.js
@@ -0,0 +1,48 @@
+const Food = require('../models/Food');
+
+/**
+ * Searches through the Food collection
+ * @param {object} criteria An object with a name, age, and yearsActive
+ * @param {string} sortProperty The property to sort the results by
+ * @param {integer} offset How many records to skip in the result set
+ * @param {integer} limit How many records to return in the result set
+ * @return {promise} A promise that resolves with the Foods, count, offset, and limit
+ * like this: { all: [Foods], count: count, offset: offset, limit: limit }
+ */
+module.exports = (criteria, sortProperty, offset = 0, limit = 20) => {
+  const query = Food.find(buildQuery(criteria))
+    .sort({ [sortProperty]: 1 })
+    .skip(offset)
+    .limit(limit);
+
+  return Promise.all([query, Food.count()])
+    .then((results) => {
+      return {
+        all: results[0],
+        count: results[1],
+        offset: offset,
+        limit: limit
+      };
+    });
+};
+
+const buildQuery = (criteria) => {
+  let query = {};
+
+  if (criteria.name) {
+    query.$text = { $search: criteria.name };
+  }
+
+  if (criteria.age) {
+    query = { age: { $gte: criteria.age.min, $lte: criteria.age.max } };
+  }
+
+  if (criteria.yearsActive) {
+    query.yearsActive = {
+      $gte: criteria.yearsActive.min,
+      $lte: criteria.yearsActive.max
+    };
+  }
+
+  return query;
+};
